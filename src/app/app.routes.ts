071be9babe43e6645e69d830163395127e08fb14
Timur Tuleuov <timmmur123@gmail.com>
2024-04-19 01:07:12 +0500

    final version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { BoardComponent } from './component/board/board.component';
 import { TimelineComponent } from './component/timeline/timeline.component';
 import { TaskComponent } from './component/task/task.component';
 import { TaskListComponent } from './component/task-list/task-list.component';
+import { AboutComponent } from './component/about/about.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'board', pathMatch: 'full' },
     {path: 'board', component: BoardComponent},
     {path: 'timeline', component: TimelineComponent},
     {path: 'task-list', component: TaskListComponent},
-    {path: 'task/:slug', component: TaskComponent}
+    {path: 'task/:slug', component: TaskComponent},
+    {path: 'about', component: AboutComponent}
 ];
